bd1e36d8c80e54249e4380d9b0a0029e11552c3b
Afonso Rodrigues <afonso.rodrigues@meliuz.com.br>
2022-01-07 17:23:32 -0300

    montando lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+interface IFimes {
+  key: number;
+  title: string;
+  released: string;
+  genre: string;
+  plot: string;
+  poster: string;
+  rating: number;
+}
 
 const Home = () => {
+  const db: IFimes[] = require('../../../db.json');
+
   return (
     <SafeAreaView>
-      <Text>Hello, World!</Text>
+      <Text style={{fontSize: 35, fontWeight: 'bold', textAlign: 'center'}}>
+        Filmes
+      </Text>
+      <FlatList
+        data={db}
+        style={{marginHorizontal: 15}}
+        renderItem={({item}) => (
+          <TouchableOpacity style={{flexDirection: 'row', marginVertical: 10}}>
+            <Image
+              source={{uri: item.poster}}
+              style={{width: 120, height: 120}}
+            />
+            <View style={{padding: 15}}>
+              <Text style={{fontSize: 23, fontWeight: 'bold'}}>
+                {item.title}
+              </Text>
+              <Text>{item.genre}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
   );
 };
