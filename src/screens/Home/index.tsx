ff51aa81db41e81b5fba84ac7defd6650bbd75ee
Afonso Rodrigues <afonso.rodrigues@meliuz.com.br>
2022-01-07 15:30:13 -0300

    configurando TS

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello, World!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
